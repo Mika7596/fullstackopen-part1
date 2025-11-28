e6281011a2c2454c53ccfa847cfa0f02eff20a8a
Mika7596 <mika7596@gmail.com>
2025-11-28 18:52:27 +0100

    Finished exercise 1.8

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
 import { useState } from "react"
 
+function Statistics ({good, bad, neutral}) {
+  return (
+    <>
+    <h2>Statistics:</h2>
+    <p>Good: {good}</p>
+    <p>Neutral: {neutral}</p>
+    <p>Bad: {bad}</p>
+    <p>Total: {good + bad + neutral}</p>
+    <p>Average: {(good || bad || neutral) ? (bad*-1 + good)/(good + bad + neutral) : 0}</p>
+    <p>Percentage of positive feedback: {good ? good*100/(good+bad+neutral) : 0}%</p>
+    </>
+  )
+}
+
+
+
 function App() {
  const [good, setGood] = useState(0);
  const [bad, setBad] = useState(0);
@@ -13,13 +29,7 @@ function App() {
     <button onClick={() => setGood(good + 1)}>Good!</button>
     <button onClick={() => setNeutral(neutral + 1)}>Neutral -_-</button>
     <button onClick={() => setBad(bad + 1)}>Bad :/</button>
-    <h2>Statistics:</h2>
-    <p>Good: {good}</p>
-    <p>Neutral: {neutral}</p>
-    <p>Bad: {bad}</p>
-    <p>Total: {good + bad + neutral}</p>
-    <p>Average: {(good || bad || neutral) ? (bad*-1 + good)/(good + bad + neutral) : 0}</p>
-    <p>Percentage of positive feedback: {good ? good*100/(good+bad+neutral) : 0}%</p>
+    <Statistics good={good} bad={bad} neutral={neutral}></Statistics>
     </>
   )
 }
